a62cc307ede341d4c4afe4b7b26a949bc0fd029b
Rafael Hurpia da Rocha <rafael@hurpia.com.br>
2018-05-02 10:51:59 -0300

    static react application with bootstrap

---
+++ front/src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-inverse navbar-fixed-top">
+        <div className="container">
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+            <a className="navbar-brand">Wall App</a>
+          </div>
+        </div>
+
+        
+      </nav>
+    )
+  }
+}
+
+class Main extends Component {
+
+  render() {
+    return (
+      <div className="container">
+
+        <div className="jumbotron">
+          <h2>My first wall message</h2>
+          <p>This is my first wall message. It is static and in the source-code (not dynamic from any database).</p>
+        </div>
+
+      </div>
+    )
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Main />
+      </div>
+    );
+  }
+}
+
+export default App;
