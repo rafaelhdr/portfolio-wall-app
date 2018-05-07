5e3812c0c681902169b1e3e2380e0242973db41b
Rafael Hurpia da Rocha <rafael@hurpia.com.br>
2018-05-07 15:39:16 -0300

    wait-for-it for e2e tests

--- e2e/test.sh
+++ e2e/test.sh
@@ -3,7 +3,10 @@
 CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BASE_DIR="$CUR_DIR/.."
 
+# Wait host to be ready
 export REACT_APP_BACKEND_HOST=${REACT_APP_BACKEND_HOST:-"http://localhost:3000"}
+host=`echo ${REACT_APP_BACKEND_HOST} | sed 's/https\?:\/\///'`
+./wait-for-it ${host}
 
 # Clear DB for tests
 sqlite3 ${BASE_DIR}/api/db.sqlite3 "DELETE FROM  auth_user WHERE username='mocktest_george'";
